61276d17fe8b3b9b5de69769c32d77d680587e3b
george-shammar <george.gbenle@outlook.com>
2023-10-03 05:41:20 +0100

    Implement reaction

--- src/views/dashboard/app/like.js
+++ src/views/dashboard/app/like.js
@@ -1,16 +1,66 @@
 import React,{useEffect, useState} from 'react';
 import CustomToggle from '../../../components/dropdowns';
-import { Dropdown } from 'react-bootstrap';
-import { ReactionTypes, usePublication, useReaction } from '@lens-protocol/react-web';
+import {Row, Col, Container, Dropdown, Nav, Tab, OverlayTrigger, Tooltip, Button, Modal } from 'react-bootstrap'
+import { ReactionTypes, useReaction } from '@lens-protocol/react-web';
 
-const Like =() => {
+import icon1 from '../../../assets/images/icon/01.png'
+import icon2 from '../../../assets/images/icon/02.png'
+import icon3 from '../../../assets/images/icon/03.png'
+import icon4 from '../../../assets/images/icon/04.png'
+import icon5 from '../../../assets/images/icon/05.png'
+import icon6 from '../../../assets/images/icon/06.png'
+import icon7 from '../../../assets/images/icon/07.png'
+
+const Like =({id, publication}) => {
+
   const { addReaction, removeReaction, hasReaction, isPending, error } = useReaction({
     profileId: id,
   });
 
+  const reactionType = ReactionTypes.Upvote;
+
+  const hasReactionType = hasReaction({
+    reactionType,
+    publication,
+  });
+
+  const toggleReaction = async () => {
+    if (hasReactionType) {
+      await removeReaction({
+        reactionType,
+        publication,
+      });
+    } else {
+      await addReaction({
+        reactionType,
+        publication,
+      });
+    }
+  };
 
   return (
-    <div>
+    <div className="d-flex align-items-center">
+      {error && <p>{error.message}</p>}
+
+      {/* <button onClick={toggleReaction} disabled={isPending}>
+        {hasReactionType ? `Remove ${reactionType}` : `Add ${reactionType}`}
+      </button> */}
+      <div className="like-data">
+        <Dropdown>
+          <Dropdown.Toggle  as={CustomToggle} >
+            <img loading="lazy" src={icon1} className="img-fluid" alt=""/>
+          </Dropdown.Toggle>
+          <Dropdown.Menu className=" py-2">
+            <OverlayTrigger placement="top" overlay={<Tooltip>Like</Tooltip>} className="ms-2 me-2" ><img loading="lazy" src={icon1} className="img-fluid me-2" alt=""/></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip>Love</Tooltip>} className="me-2" ><img loading="lazy" src={icon2} className="img-fluid me-2" alt=""/></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip>Happy</Tooltip>} className="me-2" ><img loading="lazy" src={icon3} className="img-fluid me-2" alt=""/></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip>HaHa</Tooltip>} className="me-2" ><img loading="lazy" src={icon4} className="img-fluid me-2" alt=""/></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip>Think</Tooltip>} className="me-2" ><img loading="lazy" src={icon5} className="img-fluid me-2" alt=""/></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip>Sade</Tooltip>} className="me-2" ><img loading="lazy" src={icon6} className="img-fluid me-2" alt=""/></OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip>Lovely</Tooltip>} className="me-2" ><img loading="lazy" src={icon7} className="img-fluid me-2" alt=""/></OverlayTrigger>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
          <div className="total-like-block ms-2 me-3">
             <Dropdown>
               <Dropdown.Toggle as={CustomToggle}  id="post-option" >
--- src/views/dashboard/app/user-profile.js
+++ src/views/dashboard/app/user-profile.js
@@ -38,15 +38,6 @@ import user16 from '../../../assets/images/user/16.jpg'
 import user17 from '../../../assets/images/user/17.jpg'
 import user18 from '../../../assets/images/user/18.jpg'
 import user19 from '../../../assets/images/user/19.jpg'
-import p1 from '../../../assets/images/page-img/p1.jpg'
-import p3 from '../../../assets/images/page-img/p3.jpg'
-import icon1 from '../../../assets/images/icon/01.png'
-import icon2 from '../../../assets/images/icon/02.png'
-import icon3 from '../../../assets/images/icon/03.png'
-import icon4 from '../../../assets/images/icon/04.png'
-import icon5 from '../../../assets/images/icon/05.png'
-import icon6 from '../../../assets/images/icon/06.png'
-import icon7 from '../../../assets/images/icon/07.png'
 import g1 from '../../../assets/images/page-img/g1.jpg'
 import g2 from '../../../assets/images/page-img/g2.jpg'
 import g3 from '../../../assets/images/page-img/g3.jpg'
@@ -98,9 +89,6 @@ const UserProfile =() =>{
    const [publicationId, setPublicationId] = useState("");
    let args = {};
    const { comments, hasMore, next } = useComments(args);
-   const reactionType = ReactionTypes.Upvote;
-
-
 
    const [imageController, setImageController] = useState({
       toggler: false,
@@ -130,10 +118,8 @@ const UserProfile =() =>{
     useEffect(() => {
       publications = publications?.map((publication) => {
          if (publication.__typename === "Mirror") {
-            setPublicationId(publication.mirrorOf.id);
            return publication.mirrorOf;
          } else {
-            setPublicationId(publication.id);
            return publication;
          }
        });
@@ -662,57 +648,7 @@ const UserProfile =() =>{
                                              <div className="comment-area mt-3">
                                                 <div className="d-flex justify-content-between align-items-center flex-wrap">
                                                    <div className="like-block position-relative d-flex align-items-center">
-                                                      <div className="d-flex align-items-center">
-                                                         <div className="like-data">
-                                                            <Dropdown>
-                                                               <Dropdown.Toggle  as={CustomToggle} >
-                                                                  <img loading="lazy" src={icon1} className="img-fluid" alt=""/>
-                                                               </Dropdown.Toggle>
-                                                               <Dropdown.Menu className=" py-2">
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>Like</Tooltip>} className="ms-2 me-2" ><img loading="lazy" src={icon1} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>Love</Tooltip>} className="me-2" ><img loading="lazy" src={icon2} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>Happy</Tooltip>} className="me-2" ><img loading="lazy" src={icon3} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>HaHa</Tooltip>} className="me-2" ><img loading="lazy" src={icon4} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>Think</Tooltip>} className="me-2" ><img loading="lazy" src={icon5} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>Sade</Tooltip>} className="me-2" ><img loading="lazy" src={icon6} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                                  <OverlayTrigger placement="top" overlay={<Tooltip>Lovely</Tooltip>} className="me-2" ><img loading="lazy" src={icon7} className="img-fluid me-2" alt=""/></OverlayTrigger>
-                                                               </Dropdown.Menu>
-                                                            </Dropdown>
-                                                         </div>
-                                                         <Like />
-                                                         {/* <div className="total-like-block ms-2 me-3">
-                                                            <Dropdown>
-                                                               <Dropdown.Toggle as={CustomToggle}  id="post-option" >
-                                                                  140 Likes
-                                                               </Dropdown.Toggle>
-                                                               <Dropdown.Menu>
-                                                                     <Dropdown.Item  to="#">Max Emum</Dropdown.Item>
-                                                                     <Dropdown.Item  to="#">Bill Yerds</Dropdown.Item>
-                                                                     <Dropdown.Item  to="#">Hap E. Birthday</Dropdown.Item>
-                                                                     <Dropdown.Item  to="#">Tara Misu</Dropdown.Item>
-                                                                     <Dropdown.Item  to="#">Midge Itz</Dropdown.Item>
-                                                                     <Dropdown.Item  to="#">Sal Vidge</Dropdown.Item>
-                                                                     <Dropdown.Item  to="#">Other</Dropdown.Item>
-                                                               </Dropdown.Menu>
-                                                            </Dropdown>
-                                                         </div> */}
-                                                      </div>
-                                                      {/* <div className="total-comment-block">
-                                                      <Dropdown>
-                                                         <Dropdown.Toggle as={CustomToggle}  id="post-option" >
-                                                         56 comments
-                                                         </Dropdown.Toggle>
-                                                         <Dropdown.Menu>
-                                                            <Dropdown.Item  to="#">Max Emum</Dropdown.Item>
-                                                            <Dropdown.Item  to="#">Bill Yerds</Dropdown.Item>
-                                                            <Dropdown.Item  to="#">Hap E. Birthday</Dropdown.Item>
-                                                            <Dropdown.Item  to="#">Tara Misu</Dropdown.Item>
-                                                            <Dropdown.Item  to="#">Midge Itz</Dropdown.Item>
-                                                            <Dropdown.Item  to="#">Sal Vidge</Dropdown.Item>
-                                                            <Dropdown.Item  to="#">Other</Dropdown.Item>
-                                                         </Dropdown.Menu>
-                                                      </Dropdown>
-                                                      </div> */}
+                                                      <Like id={id} publication={pub} />
                                                    </div>
                                                 <ShareOffcanvas />
                                                 </div>
